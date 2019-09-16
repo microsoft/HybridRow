956e44989a87de03a8cbf1be100421da184d1d74
David Noble <David.Noble@microsoft.com>
2019-09-15 21:27:44 -0700

    Removed dead code

--- java/src/main/java/tangible/Func1Param.java
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-package tangible;
-
-@FunctionalInterface
-public interface Func1Param<T, TResult> {
-    TResult invoke(T t);
-}
