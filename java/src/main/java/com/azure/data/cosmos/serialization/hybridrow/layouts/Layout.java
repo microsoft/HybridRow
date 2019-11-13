ec7519f92912ec489925f0e25a2ae9ce60841497
Mohammad Derakhshani <moderakh@users.noreply.github.com>
2019-11-13 10:10:43 -0800

    capture and log exception during static initializatoin

--- java/src/main/java/com/azure/data/cosmos/serialization/hybridrow/layouts/Layout.java
+++ java/src/main/java/com/azure/data/cosmos/serialization/hybridrow/layouts/Layout.java
@@ -10,6 +10,8 @@ import com.azure.data.cosmos.serialization.hybridrow.schemas.Namespace;
 import com.azure.data.cosmos.serialization.hybridrow.schemas.Schema;
 import com.azure.data.cosmos.serialization.hybridrow.schemas.StorageKind;
 import com.google.common.collect.ImmutableList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nonnull;
 import java.util.ArrayList;
@@ -33,8 +35,17 @@ import static com.google.common.base.Preconditions.checkNotNull;
  * {@link Layout} is immutable.
  */
 public final class Layout {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Layout.class);
 
-    public static final Layout EMPTY = SystemSchema.layoutResolver().resolve(SystemSchema.EMPTY_SCHEMA_ID);
+    public static final Layout EMPTY;
+    static {
+        try {
+            EMPTY = SystemSchema.layoutResolver().resolve(SystemSchema.EMPTY_SCHEMA_ID);
+        } catch (Throwable t) {
+            LOGGER.error("Unexpected failure during static initialization", t);
+            throw t;
+        }
+    }
 
     private final String name;
     private final int numBitmaskBytes;
