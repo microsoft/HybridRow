f0a899c346ff207f84307d26caf790f82a3ab42a
Jason Hunter <jthunter@microsoft.com>
2021-05-05 14:03:07 -0700

    delete old dotnet snapshot

--- dotnet/src/HybridRow/GlobalSuppressions.cs
+++
@@ -1,9 +0,0 @@
-﻿// ------------------------------------------------------------
-//  Copyright (c) Microsoft Corporation.  All rights reserved.
-// ------------------------------------------------------------
-
-[assembly:
-    System.Diagnostics.CodeAnalysis.SuppressMessage(
-        "StyleCop.CSharp.LayoutRules",
-        "SA1512:Single-line comments should not be followed by blank line",
-        Justification = "Refactoring")]
